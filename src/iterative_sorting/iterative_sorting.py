681b25681a1dfae59d126710e3c99fff94880190
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-12-10 08:49:35 -0600

    finished MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -13,18 +13,21 @@ def selection_sort( arr ):
         # TO-DO: swap
         arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
-
+print(selection_sort([9,3,5,4,7,5,1]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for i in range(0, len(arr)-1):
+    for i in range(0, len(arr)):
         cur_index = i
-        smallest_index = cur_index
-        for num in range(cur_index, cur_index - n ):
-            if arr[num]
+        for num in range(0,  cur_index):
+            if arr[num] > arr[num+1]:
+                isSorted = True
+                arr[num+1], arr[num] = arr[num], arr[num + 1]
+                if isSorted :
+                    bubble_sort(arr)
     return arr
 
-
+print(bubble_sort([9,3,5,4,7,5,1]))
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
